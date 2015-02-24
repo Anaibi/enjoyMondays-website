167e9afaf2f0f67a3785549471043523a6e99591
Anaibi <burgostatiana@yahoo.com>
2015-02-24 13:16:05 +0100

    messing around with waypoint calls for loading page fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,17 +12,24 @@ $(function() {
 		return $(window).height();
 	};
 
-	var i=1; console.log(i);
+	var no_of_items = sessionStorage.length;  console.log(no_of_items); 
+	var page = 'home'; console.log(page);
+
+	var $firstLoad = $('#firstLoad'); console.log($firstLoad);
 
 ////////////////////////////////////////
 ////////////////////////////////////////
 
 	//first thing, layout: fit display to window height and set layout according to dimensions/device
-	setPageHeight();
-	setLayout();
+	$('body').fadeOut("fast", function() {
+		setPageHeight();
+		setLayout();
+		//$firstLoad.text('false'); console.log($firstLoad);
+		//callWaypoint();
+	}).fadeIn("fast");
 	
 	//on changing window size, fit again
-	$(window).resize(function() { console.log('in window resize');
+	$(window).resize(function() { 
 		setPageHeight();
 		setLayout();	
 	});	
@@ -34,12 +41,14 @@ $(function() {
 	//call when entering new section to update links, special cases home section and footer
 
 	//when scrolling in and out of home section, and offset HEADER_HEIGHT - 1 
-	$('#home header').waypoint(function(direction) {		
+	//$firstLoad.change(function() { console.log('firstLoad changed'); //this doesn't work
+	//function callWaypoint() { //this makes a mess
+		$('#home header').waypoint(function(direction) {		
 		//scrolling out
 		if (direction === 'down') { //running this on page reload, it shouldn't
-			if (isLandscapeHeader() && !isPage('home')) {  console.log('in waypoint home down with offset, hide logo');
+			if (isLandscapeHeader() && !isPage('home')) {  console.log('in waypoint home down with offset, hide logo ' + page);
 				hideLogo();
-			} else { console.log('in waypoint home, down, small logo ');
+			} else { console.log('in waypoint home, down, small logo ' + page);
 				smallLogo(); 
 			};
 			moveMenu('moveUp');
@@ -48,8 +57,8 @@ $(function() {
 		else if (direction === 'up') {
 			if (isLandscapeHeader()) {
 				showLogo();
-				smallLogo();	
-			} else { console.log('in waypoint home, up, big logo ');
+				smallLogo(); console.log('in waypoing direction up, offset, ' + page);	
+			} else { console.log('in waypoint home, up, big logo ' + page);
 				bigLogo();
 			};		
 			moveMenu('moveDown');
@@ -58,16 +67,21 @@ $(function() {
 
 	//when scrolling into home section from works section, and no offset
 	$('#home header').waypoint(function(direction) {
-		if (direction === 'up')
+		if (direction === 'up') {
+			console.log('in waypoint called from home header, direction up, page ' + page);
 			updateLinks('#home');
+		}
 	});
 		
 	//when scrolling down into new section, at the middle of the section
 	//for pages #work, #about, #contact	
-	$('.page header').waypoint(function(direction) { 		 
+	$('.page header').waypoint(function(direction) { 	 
 		if (direction === 'down') {
-			var page = '#' + $(this).parent().attr('id');
+			console.log('in waypoint function, direction down, from .page header ' + page);
+			//var page = '#' + $(this).parent().attr('id');
+			page = '#' + $(this).parent().attr('id');
 			updateLinks(page);
+			console.log('in waypoint direction down, from .page header, after update links called, page ' + page);
 		}
 	}, {offset: '50%'});
 	
@@ -75,30 +89,35 @@ $(function() {
 	//for pages #work and #about
 	$('.page').waypoint(function(direction) {
 		if (direction == 'up') {
-			var page = '#' + $(this).attr('id');
+		console.log('in waypoint function, direction up, from .page ' + page);
+			//var page = '#' + $(this).attr('id');
+			page = '#' + $(this).attr('id');
 			updateLinks(page);
+			console.log('in waypoint direction up, from .page, after update links called, page ' + page);
 		}
 	}, {offset: 'bottom-in-view'});	
 
 	//when scrolling in or out of last section, show or hide footer
 	$('#contact .sub-header').waypoint(function(direction) { 
 		if (direction === 'down') {
+		console.log('in waypoint function, direction down, from #contact sub-header ' + page);
 			showFooter();
 		} 
 		else if (direction === 'up') {
 			hideFooter();
+			console.log('in waypoint direction up, from #contact sub-header, after update links called, page ' + page);
 		}
 	}, {offset: 'bottom-in-view'});	
 
-
+//};
 //////////////////////////////////////// MENU FUNCTIONALITY
 ////////////////////////////////////////
 	
 	//main menu navigation
 	$('nav a').click(function(e){ 
 		e.preventDefault();
-		var page = $(this).attr('href'); 
-
+		//var page = $(this).attr('href'); 
+		page = $(this).attr('href'); 
 		updateLinks(page);
 		
 		if(!isPage('notes')) scrollToPosition(page);								
@@ -166,7 +185,7 @@ $(function() {
 
 	//////////////////////////////////////// PAGES / LAYOUT SETTINGS AND HELPER FUNCTIONS
 	//set height for pages
-	function setPageHeight() { 
+	function setPageHeight() { console.log($firstLoad);
 		//efective space for content
 		var h = windowHeight() - HEADER_HEIGHT; 
 		
@@ -187,13 +206,13 @@ $(function() {
 	
 	
 	// resolve layout
-	function setLayout() {  console.log(i);
+	function setLayout() {  console.log($firstLoad);
 
 		// check if is landscape or fixed header
 		setHeaderClass();
 
-		var page = $('.active-link').attr('href'); 
-
+		//var page = $('.active-link').attr('href'); 
+		page = $('.active-link').attr('href');
 		
 		// unless on contact page, FOOTER is hidden untill contact page reached
 		if (!isPage('contact')) hideFooter(); //was used for slideUp, if not used take out
@@ -203,14 +222,11 @@ $(function() {
 
 		// set HEADER and PAGE position 
 		//if (!isLandscapeHeader()) {
-		if (i) {
-			setTimeout(function() { 
-				fixHeader(); 
-				scrollToPosition(page);
-	   		}, 400);
-		//}
-		}
-		i = false;
+		setTimeout(function() { 
+			fixHeader(); 
+			scrollToPosition(page);	
+	   	}, 600);
+		
 	};
 	
 	// set header, can be fixed (desktops and portrait mobile) --> do collapsed menu for landscape mobile?
@@ -228,15 +244,12 @@ $(function() {
 
 	// set fixed header
 	function fixHeader() { 
-		/*var header = $('#main-header header');	
-		var h;
 		if (isLandscapeHeader()) {
-			h = $('#main-header nav').height() + parseInt(header.css('padding-bottom'));
+			var $nav = $('nav');
+			$('#fixed-header-aux').height($nav.height() + parseInt($nav.css('margin-top')) + parseInt($nav.css('margin-bottom')));
 		} else {
-			h = header.height() + parseInt(header.css('padding-bottom'));
+			$('#fixed-header-aux').height($('#main-header').height());
 		}
-		$('.fixed-fixed').height(h);*/
-
 	};
 	
 
@@ -250,7 +263,7 @@ $(function() {
 	}
 
 	//scroll to pages position
-	function scrollToPosition(page) {	console.log('scroll to position called');	
+	function scrollToPosition(page) {	console.log('scroll to position called ' + page);	
 		//con overflow-x hidden el scrollTop no funciona
 		$('html, body').css('overflow-x', 'visible'); 
 
