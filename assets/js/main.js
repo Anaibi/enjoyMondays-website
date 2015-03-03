4af3a80dba14d7f509bc795a5729c1b85d896fb1
Anaibi <burgostatiana@yahoo.com>
2015-03-03 11:07:39 +0100

    checking devices for responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,16 +19,18 @@ $(function() {
 
 	// fit display to window height and set layout according to dimensions/device
 	// to calculate heights wait untill all content loaded
-	$(window).load(function () {
+	$(window).load(function () { 
     	setLayout(function() {
     		setTimeout(function() {
     			doWaypoints();
-    			doHeader('home', 'up');
-    		}, 500);
+    			centerContents();
+    		}, 100);
     	});
+    	$('html').animate({
+    		opacity: 1
+    	});		
 	});	
 	
-	// TODO not doing contact page height recalculation 
 	$(window).resize(function() {  console.log('resize');
 		setLayout();
 		Waypoint.refreshAll();
@@ -48,196 +50,138 @@ $(function() {
 $('nav a').click(function(e){  
 	e.preventDefault();
 
-	//get clicked page 
-	page = $(this).attr('href'); 
-
-	scrollToPosition(page);
+	scrollToPosition($(this).attr('href'));
 	
 });			
 
 // notes link off	
 $('nav a[href="#notes"]').off('click');
 
-//////////////////////////////////////// WAYPOINT PLUGIN
+//////////////////////////////////////// WAYPOINT PLUGIN implementation
 //////////////////////////////////////// http://imakewebthings.com/waypoints/
 
 function doWaypoints() { 
-// HOME scrolling UP
-var waypointHomeUp = new Waypoint({
-  element: document.getElementById('home'),
-  handler: function(direction) { 
-  	doHeader('home', direction);
-  },
-  offset: 'bottom-in-view'
-});
+	// HOME scrolling UP
+	var waypointHomeUp = new Waypoint({
+	  element: document.getElementById('home'),
+	  handler: function(direction) { 
+	  	doHeader('home', direction);
+	  },
+	  offset: 'bottom-in-view'
+	});
 
-// HOME scrolling DOWN
-// small logo and menu up
-// TODO refactor into one function deals with all header (logo + nav) animations
-var waypointHomeUp = new Waypoint({
-  element: document.getElementById('home'),
-  handler: function(direction) {
-  	doHeader('home', direction)
-  }
-});
+	// HOME scrolling DOWN
+	var waypointHomeUp = new Waypoint({
+	  element: document.getElementById('home'),
+	  handler: function(direction) {
+	  	doHeader('home', direction)
+	  }
+	});
 
-// CONTACT : hide/show footer
-var waypointContact = new Waypoint({
-  element: document.getElementById('contact'),
-  handler: function(direction) { 
-	if (direction === 'down') { 
-		showFooter();
-	} else {
-		hideFooter();
-	}; 
-  },
-  offset: 'bottom-in-view'
-});
+	// CONTACT : hide/show footer
+	var waypointContact = new Waypoint({
+	  element: document.getElementById('contact'),
+	  handler: function(direction) { 
+		if (direction === 'down') { 
+			showFooter();
+		} else {
+			hideFooter();
+		}; 
+	  },
+	  offset: 'bottom-in-view'
+	});
 
-var $pages = $('.page');
+	// PAGES : update nav links and set header height
+	var $pages = $('.page');
 
-$pages.each(function() { // update nav links
-	new Waypoint({
-    	element: this,
-      	handler: function(direction) { 
-        	var previousWaypoint = this.previous(); 
+	$pages.each(function() { 
+		new Waypoint({
+	    	element: this,
+	      	handler: function(direction) { 
+	        	
+	        	var previousWaypoint = this.previous(); 
 
-        	$pages.removeClass('.active-link');
+	        	$pages.removeClass('.active-link');
 
-        	if (direction === 'down') {
-        		updateLinks($(this.element).attr('id'));
-        	} 
-        	if (previousWaypoint && (direction === 'up')) {
-        		updateLinks($(previousWaypoint.element).attr('id'));
-        	}
-      	},
-      	offset: '50%',
-      	group: 'page'
+	        	if (direction === 'down') { updateLinks($(this.element).attr('id')); } 
+	        	
+	        	if ((direction === 'up') && previousWaypoint) { updateLinks($(previousWaypoint.element).attr('id')); }
+
+	        	setHeaderHeight();
+	      	},
+	      	offset: '50%',
+	      	group: 'pages'
+		})
 	})
-})
 }
+
 
 // FUNCTIONS	
 
 //////////////////////////////////////// PAGES / LAYOUT SETTINGS AND HELPER FUNCTIONS
+
 // RESOLVE LAYOUT
 function setLayout(callback) {  
 
-	// check if is landscape or fixed header
+	// header can be regular = .header-1, mobile landscape = .header-2 or mobile portrati = .header-3
 	setHeaderClass();
-
-	hideFooter(); //was used for slideUp, if not used take out
 		
 	// set page height to fill window 
 	setPageHeight();
 
-	// center page/section contents (y axis)
-	centerContents();	
-
-	if (typeof callback == 'function') { 
-        callback.call(this); 
-    }
+	if (typeof callback == 'function') { callback.call(this); }
 }
+
+// SET HEIGHT for PAGES to Fill Window -- fix spaces: header, footer
+function setPageHeight(callback) { 
+
+	var main_header_h = $('#main-header').height(),
+		window_h = windowHeight(),
+		footer_h = $('footer').height(),
+		$contact_page = $('#contact');
 	
-// set header, can be fixed (desktops and portrait mobile) or landscape
-function setHeaderClass() { 
+	// simple case just make sure page + header fills window
+	$('.page').css('min-height', window_h - main_header_h);
+
+	// CONTACT PAGE: if footer fits into page, set height = h - footer height
+	if ((main_header_h + $('#contact header').height() + footer_h) <= window_h) { 
+		
+		$('#contact').css({
+			'height': (window_h - main_header_h - footer_h),
+			'min-height': (window_h - main_header_h - footer_h)
+		});
 	
-	// check caveats described here for further tuning
-	// http://alxgbsn.co.uk/2012/08/27/trouble-with-web-browser-orientation/
-
-	if ((windowWidth() >= windowHeight()) && (windowWidth() <= WINDOW_WIDTH_MARK)) { // Landscape
-		$("html").addClass('landscape, header-2').removeClass('header-1, header-3'); 
-	} else  if ((windowWidth() <= windowHeight()) && (windowHeight() <= WINDOW_HEIGHT_MARK)) { // Portrait header-three
-		$("html").removeClass('landscape, header-2, header-1').addClass('header-3');  
 	} else {
-		$("html").removeClass('landscape, header-2, header-3').addClass('header-1'); 
-	}
-}
-
-// do header, can be fixed or landscape
-// TODO 
-// * on mobile (google nexus) header height varies depending on browser? device? 
-// * on landscape orientation logo stays centered and menu fills all layout
-// * case menu doesnt't fit in so shifts down (either css or script)
-// possible headers:
-// 1. big logo at left, menu at right --> for home page when width and height allow for it. 
-//    on scroll, logo animates to small and menu moves up
-// 2. small logo at top, menu under logo, centered --> for home page when width < xx and height > yy 
-//    On scroll, header slides up and only small menu bar visible
-// 3. small logo at left and menu at right --> for home page when landscape orientation and height > yy
-//    on scroll, header slides to left, logo hidden and menu centered
-function doHeader(page, direction) {
- 	if (page.charAt(0) === '#') {
- 		page = page.trim('#');
-	}
-	if (isHeader(1)) { // home big/small logo left, menu right dowb/up
-		if (direction === 'up') {
-			bigLogo();
-  			moveMenu('down');
-		} else {
-			showLogo('right');
-			smallLogo();
-  			moveMenu('up');
-		}
-	} else if (isHeader(2)) { // landscape
-		if (direction === 'up') {
-			showLogo('right');
-		} else {
-			hideLogo('left');
-		}
-	} else if (isHeader(3)) { // portrait
-		if (direction === 'up' && page === 'home') { 
-			smallLogo();
-			showLogo('down');
-			moveMenu('up');
-		} else {
-			hideLogo('up');
-			moveMenu('up');
-		}
-	}
-	setHeaderHeight();
-}
-
-function setHeaderHeight() { console.log('doing header height');
-	console.log(isPage('home'));
-	if ((isHeader(2) || isHeader(3)) && !isPage('home')) {
-		var $nav = $('nav');
-		console.log($nav.height() + parseInt($nav.css('margin-top')) + parseInt($nav.css('margin-bottom')));
-		$('#fixed-header-aux, #main-header').height($nav.height() + parseInt($nav.css('margin-top')) + parseInt($nav.css('margin-bottom')));
-	} else {
-		$('#fixed-header-aux, #main-header').height(HEADER_HEIGHT);
-	}
-}
-// SET HEIGHT for PAGES to Fill Window
-// fix spaces: header, footer
-function setPageHeight() { 
-	//efective space for content
-	var h = windowHeight() - $('#main-header').height(); 
 		
-	//save contact page height before modifying
-	var contactPageHeight = $('#contact.page').height()
-		
-	//simple case just make sure page + header fills window
-	$('.page').css('min-height', h);
-
-	//check if footer fits into page contact, if it does, set height = h - footer height
-	if ((HEADER_HEIGHT + contactPageHeight + $('footer').height()) <= windowHeight()){
-		$('#contact.page').css('height', (h - $('footer').height()));
-		$('#contact.page').css('min-height', (h - $('footer').height()));
-	} else {
-		$('#contact.page header h1')
+		console.log('fit page height')
 	}
-	// if it doesn't, min height = h and footer has to deal with that?
 }
 
 //center home and contact page content
 function centerContents() { 
-	//set top margin for each aprox 50% of free space
-	var yH = $('#home').height() - $('#home header').height(); 
-	var cH = $('#contact').height() - $('#contact header').height() - $('footer').height(); 
-	$('#home header').css('margin-top', (yH/3));
-	$('#contact header').css('margin-top', (cH/3));
+	var window_h = windowHeight(),
+		header_h = $('#main-header').height();
+	
+	// home
+	var home_h = $('#home').height(),
+		$home_header = $('#home header');
+
+	if (header_h + home_h <= window_h) {
+		$home_header.css('margin-top', ((home_h - $home_header.height())/3));
+	}
+
+	// contact
+	var contact_h= $('#contact').height(),
+		footer_h = $('footer').height(),
+		$contact_header = $('#contact header');
+	
+	if (header_h + contact_h + footer_h <= window_h) { 
+		$contact_header.css('margin-top', (contact_h - $contact_header.height())/3);
+	} else {
+		console.log('center contentt')
+	}
 }
+
 
 //scroll to pages position
 function scrollToPosition(page) { 
@@ -254,17 +198,91 @@ function scrollToPosition(page) {
 }
 
 // return true if is active page
-function isPage(id) {  console.log(id); console.log($('.active-link').attr('href'));
+function isPage(id) {  
 	return ($('.active-link').attr('href') === '#' + id);
 }
 
-function isLandscapeHeader() { 
-	return $("html").hasClass('landscape');
-}
-
+// return true is is class .header-i
 function isHeader(i) { 
 	return $("html").hasClass('header-' + i);
 }
+
+// HEADER FUNCTIONS
+
+// header can be regular = .header-1, mobile landscape = .header-2 or mobile portrati = .header-3
+function setHeaderClass() { 
+	
+	// check caveats described here for further tuning
+	// http://alxgbsn.co.uk/2012/08/27/trouble-with-web-browser-orientation/
+
+	if ((windowWidth() >= windowHeight()) && (windowWidth() <= WINDOW_WIDTH_MARK)) { // Landscape .header-2
+
+		$("html").addClass('header-2').removeClass('header-1, header-3'); 
+
+	} else if ((windowWidth() <= windowHeight()) && (windowHeight() <= WINDOW_HEIGHT_MARK)) { // Portrait .header-3
+
+		$("html").removeClass('header-2, header-1').addClass('header-3'); 
+
+	} else {
+
+		$("html").removeClass('header-2, header-3').addClass('header-1'); // regular layout .header-1
+	
+	}
+}
+
+// HEADER ANIMATION
+// 1. big logo at left, menu at right --> for home page when width and height allow for it. 
+//    on scroll, logo animates to small and menu moves up
+// 2. small logo at top, menu under logo, centered --> for home page when width < xx and height > yy 
+//    On scroll, header slides up and only small menu bar visible
+// 3. small logo at left and menu at right --> for home page when landscape orientation and height > yy
+//    on scroll, header slides to left, logo hidden and menu centered
+function doHeader(page, direction) { 
+ 	
+ 	if (page.charAt(0) === '#') { page = page.trim('#'); }
+
+	if (isHeader(1)) { // home big/small logo left, menu right dowb/up
+		if (direction === 'up') {
+			bigLogo();
+  			moveMenu('down');
+		} else {
+			showLogo('right');
+			smallLogo();
+  			moveMenu('up');
+		}
+	} else if (isHeader(2)) { // 'small' landscape
+		if (direction === 'up' && page === 'home') { 
+			smallLogo();
+			showLogo('right');
+			moveMenu('up');
+		} else {
+			hideLogo('left');
+			moveMenu('up');
+		}
+	} else if (isHeader(3)) { // 'small' portrait
+		if (direction === 'up' && page === 'home') { 
+			smallLogo();
+			showLogo('down');
+			moveMenu('up');
+		} else {
+			hideLogo('up');
+			moveMenu('up');
+		}
+	}
+
+	setHeaderHeight();
+}
+
+function setHeaderHeight() { 
+	if ((isHeader(2) || isHeader(3)) && !isPage('home')) {
+		var $nav = $('nav');
+		$('#fixed-header-aux, #main-header').height($nav.height() + parseInt($nav.css('margin-top')) + parseInt($nav.css('margin-bottom')));
+	} else {
+		$('#fixed-header-aux, #main-header').height(HEADER_HEIGHT);
+	}
+}
+
+
 
 //////////////////////////////////////// LOGO ANIMATION
 //animate logo to big
@@ -281,7 +299,7 @@ function bigLogo() {
 }
 	
 //animate logo to small
-function smallLogo() { 
+function smallLogo() {  
 	
 	$('#logo').animate({
 			'margin-top': '17px'
@@ -293,19 +311,23 @@ function smallLogo() {
 }
 
 // 
-function hideLogo(direction) {
+function hideLogo(direction) {  
 	if (direction === 'up') {
 		$('#logo').slideUp();
 	} else if (direction === 'left') {
-		$('#logo').show("slide", { direction: "left" }, 1000);
+		$('#logo').animate({
+			'margin-left': '-1000px'
+		}, 2000);
 	}
 }
 
-function showLogo(direction) {
+function showLogo(direction) { 
 	if (direction === 'down') {
 		$('#logo').slideDown();
 	} else if (direction === 'right') { 
-		$('#logo').show("slide", { direction: "right" }, 1000);
+		$('#logo').animate({
+			'margin-left': '12px'
+		}, 'slow');
 	}
 }
 
